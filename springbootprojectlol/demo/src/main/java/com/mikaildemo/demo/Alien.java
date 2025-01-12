e0c104d45f60506b28e5337571112bc4a84b20f6
msyed2020 <mikailsyed@yahoo.com>
2025-01-12 13:01:20 -0500

    Add new folder

--- springbootprojectlol/demo/src/main/java/com/mikaildemo/demo/Alien.java
+++ springbootprojectlol/demo/src/main/java/com/mikaildemo/demo/Alien.java
@@ -1,10 +1,16 @@
 package com.mikaildemo.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Alien {
+
+    @Autowired
+    Laptop laptop;
+
     public void code() {
         System.out.println("Yes, I'm coding");
+        laptop.compile();
     }
 }
