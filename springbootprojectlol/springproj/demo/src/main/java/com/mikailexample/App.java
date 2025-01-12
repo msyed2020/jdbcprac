70fb1235c10f5e616f0db51d65a24198089a8190
msyed2020 <mikailsyed@yahoo.com>
2025-01-12 18:32:03 -0500

    Loading using config

--- springbootprojectlol/springproj/demo/src/main/java/com/mikailexample/App.java
+++ springbootprojectlol/springproj/demo/src/main/java/com/mikailexample/App.java
@@ -1,7 +1,10 @@
 package com.mikailexample;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.mikailexample.config.AppConfig;
 
 /**
  * Hello world!
@@ -12,13 +15,17 @@ public class App
     public static void main( String[] args )
     {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Terra terra = (Terra) context.getBean("terra");
-        terra.setPercentAwesome(100);
-        System.out.println(terra.getPercentAwesome());
-        terra.code();
-        Love l = (Love) context.getBean("love");
-        //l.setLove(0);
-        System.out.println(l.getLove());
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Terra t = context.getBean(Terra.class);
+        t.code();
+        // ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        // Terra terra = (Terra) context.getBean("terra");
+        // terra.setPercentAwesome(100);
+        // System.out.println(terra.getPercentAwesome());
+        // terra.code();
+        // Love l = (Love) context.getBean("love");
+        // //l.setLove(0);
+        // System.out.println(l.getLove());
     }
 }
--- springbootprojectlol/springproj/demo/src/main/java/com/mikailexample/config/AppConfig.java
+++ springbootprojectlol/springproj/demo/src/main/java/com/mikailexample/config/AppConfig.java
@@ -2,10 +2,12 @@ package com.mikailexample.config;
 
 import com.mikailexample.Terra;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 
 @Configuration
 public class AppConfig {
     
+    @Bean
     public Terra terra() {
         return new Terra();
     }
