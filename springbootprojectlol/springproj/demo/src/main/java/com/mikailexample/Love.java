8e410d841630de402b36743a7d58b32bd548a218
msyed2020 <mikailsyed@yahoo.com>
2025-01-12 13:35:44 -0500

    Lol

---
+++ springbootprojectlol/springproj/demo/src/main/java/com/mikailexample/Love.java
@@ -0,0 +1,7 @@
+package com.mikailexample;
+
+public class Love {
+    public Love() {
+        System.out.println("Love");
+    }
+}
--- springbootprojectlol/springproj/demo/src/main/java/com/mikailexample/Terra.java
+++ springbootprojectlol/springproj/demo/src/main/java/com/mikailexample/Terra.java
@@ -1,6 +1,9 @@
 package com.mikailexample;
 
 public class Terra {
+    public Terra() {
+        System.out.println("I love Terra");
+    }
     public void code() {
         System.out.println("I'm coding");
     }
