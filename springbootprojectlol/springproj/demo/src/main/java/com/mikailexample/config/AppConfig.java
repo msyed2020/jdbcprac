76682593b52bf01d919e6cb3460eb935d60fce5d
msyed2020 <mikailsyed@yahoo.com>
2025-01-12 21:40:49 -0500

    Add scope

--- springbootprojectlol/springproj/demo/src/main/java/com/mikailexample/config/AppConfig.java
+++ springbootprojectlol/springproj/demo/src/main/java/com/mikailexample/config/AppConfig.java
@@ -3,11 +3,13 @@ package com.mikailexample.config;
 import com.mikailexample.Terra;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
     
     @Bean
+    @Scope("prototype")
     public Terra terra() {
         return new Terra();
     }
