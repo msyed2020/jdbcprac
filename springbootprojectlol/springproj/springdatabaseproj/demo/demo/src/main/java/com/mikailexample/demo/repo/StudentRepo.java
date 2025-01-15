804d6eb4c25d6048590db40ca69de7728a48992f
msyed2020 <mikailsyed@yahoo.com>
2025-01-15 15:20:26 -0500

    Set up use before database

--- springbootprojectlol/springproj/springdatabaseproj/demo/demo/src/main/java/com/mikailexample/demo/repo/StudentRepo.java
+++ springbootprojectlol/springproj/springdatabaseproj/demo/demo/src/main/java/com/mikailexample/demo/repo/StudentRepo.java
@@ -1,7 +1,13 @@
 package com.mikailexample.demo.repo;
 
+import org.springframework.stereotype.*;
+import com.mikailexample.demo.model.*;
 
-
+@Repository
 public class StudentRepo {
+    public void save(Student s) {
 
+    }
+
+    // create a findall method to fetch data from database
 }
--- springbootprojectlol/springproj/springdatabaseproj/demo/demo/src/main/java/com/mikailexample/demo/service/StudentService.java
+++ springbootprojectlol/springproj/springdatabaseproj/demo/demo/src/main/java/com/mikailexample/demo/service/StudentService.java
@@ -1,6 +1,8 @@
 package com.mikailexample.demo.service;
 
 import com.mikailexample.demo.model.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.mikailexample.demo.repo.*;
 
@@ -10,14 +12,16 @@ public class StudentService {
     private StudentRepo repo;
 
     public void addStudent(Student s) {
-        System.out.println("Add student");
+        repo.save(s);
     }
 
     public StudentRepo getStudentRepo() {
         return repo;
     }
-
+    @Autowired
     public void setStudentRepo(StudentRepo repo) {
         this.repo = repo;
     }
+
+    // create getter for student list
 }
