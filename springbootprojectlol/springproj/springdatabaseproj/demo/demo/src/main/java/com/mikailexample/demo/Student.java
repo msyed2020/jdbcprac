861ecf243500ca76b37180fc2c8f03ab50ba281e
msyed2020 <mikailsyed@yahoo.com>
2025-01-15 14:29:26 -0500

    Add gets sets

---
+++ springbootprojectlol/springproj/springdatabaseproj/demo/demo/src/main/java/com/mikailexample/demo/Student.java
@@ -0,0 +1,32 @@
+package com.mikailexample.demo;
+
+public class Student {
+    private int id;
+    private String name;
+    private int grade;
+
+    public int getID() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+}
