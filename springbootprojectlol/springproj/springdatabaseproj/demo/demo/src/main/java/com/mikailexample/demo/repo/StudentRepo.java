92a883c810eebc54943c581fd17c287e9c2ec049
msyed2020 <mikailsyed@yahoo.com>
2025-01-15 22:51:47 -0500

    Prepare basic SQL insertion

--- springbootprojectlol/springproj/springdatabaseproj/demo/demo/src/main/java/com/mikailexample/demo/repo/StudentRepo.java
+++ springbootprojectlol/springproj/springdatabaseproj/demo/demo/src/main/java/com/mikailexample/demo/repo/StudentRepo.java
@@ -3,11 +3,30 @@ package com.mikailexample.demo.repo;
 import org.springframework.stereotype.*;
 import com.mikailexample.demo.model.*;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import java.util.*;
+
 @Repository
 public class StudentRepo {
-    public void save(Student s) {
 
+    private JdbcTemplate jdbc;
+
+    public JdbcTemplate getJdbc() {
+        return jdbc;
+    }
+    @Autowired
+    public void setJdbc(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+    
+    public void save(Student s) {
+        String sql = "insert into student (id, name, grade) values (?, ?, ?)";
+        jdbc.update(sql, s.getID(), s.getName(), s.getGrade());
     }
 
-    // create a findall method to fetch data from database
+    public List<Student> findAll() {
+        List<Student> students = new ArrayList<>();
+        return students;
+    }
 }
